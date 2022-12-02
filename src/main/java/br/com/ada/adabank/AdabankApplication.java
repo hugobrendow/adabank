391b6d2f2dc5333206b93be65a1f2a5950ce4dfa
Hugo Brendow <hugobrendow97@gmail.com>
2022-12-02 18:17:35 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/ada/adabank/AdabankApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.adabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/adabank/AdabankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.adabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
